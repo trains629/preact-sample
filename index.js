6a1e4b6fdfe3d58e0d666fa56b6fcddbaa0afb93
biger <biger@trains629.com>
2020-09-03 23:52:11 +0800

    使用bulma样式框架

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 import { h, render } from "preact";
+//import 'bulma/css/bulma.css';
+import "./styles.scss";
 
 function Card({children,caption}) {
   return <div className="card">
-    <div className="card-header">
-      {caption}
-    </div>
-    <div className="card-body">
+    <header className="card-header has-background-success">
+      <p className="card-header-title has-text-success-light">
+        {caption}
+      </p>
+      <a className="card-header-icon">
+        <span className="icon">
+          <i className="fas fa-angle-down"/>
+        </span>
+      </a>
+    </header>
+    <div className="card-content">
       {children}
     </div>
+    <footer className="card-footer">
+
+    </footer>
   </div>
 }
 
 function App() {
-  return <div className="container">
-    <Card caption="preact sample"></Card>
+  return <div className="container" style={{marginTop:"32px"}}>
+    <Card caption="preact sample">
+      <div style={{height:200}}></div>
+    </Card>
   </div>;
 }
 
